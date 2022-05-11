fe8d4a96a5ad387b376b498c0e5255390e23fc75
流照君 <50123492+zhirui-gao@users.noreply.github.com>
2022-05-11 11:23:42 +0800

    Create weighted_Rigid_3D.py

---
+++ weighted_Rigid_3D.py
@@ -0,0 +1,39 @@
+def rigid_transform_3d( A, B, weights=None, weight_threshold=0):
+    """ 
+    Input:
+        - A:       [bs, num_corr, 3], source point cloud
+        - B:       [bs, num_corr, 3], target point cloud
+        - weights: [bs, num_corr]     weight for each correspondence 
+        - weight_threshold: float,    clips points with weight below threshold
+    Output:
+        - R, t 
+    """
+    bs = A.shape[0]
+    if weights is None:
+        weights = torch.ones_like(A[:, :, 0])
+    weights[weights < weight_threshold] = 0
+    # weights = weights / (torch.sum(weights, dim=-1, keepdim=True) + 1e-6)
+
+    # find mean of point cloud
+    centroid_A = torch.sum(A * weights[:, :, None], dim=1, keepdim=True) / (torch.sum(weights, dim=1, keepdim=True)[:, :, None] + 1e-6)
+    centroid_B = torch.sum(B * weights[:, :, None], dim=1, keepdim=True) / (torch.sum(weights, dim=1, keepdim=True)[:, :, None] + 1e-6)
+
+    # subtract mean
+    Am = A - centroid_A
+    Bm = B - centroid_B
+
+    # construct weight covariance matrix
+    Weight = torch.diag_embed(weights)
+    H = Am.permute(0, 2, 1) @ Weight @ Bm
+
+    # find rotation
+    U, S, Vt = torch.svd(H.cpu())
+    U, S, Vt = U.to(weights.device), S.to(weights.device), Vt.to(weights.device)
+    delta_UV = torch.det(Vt @ U.permute(0, 2, 1))
+    eye = torch.eye(3)[None, :, :].repeat(bs, 1, 1).to(A.device)
+    eye[:, -1, -1] = delta_UV
+    R = Vt @ eye @ U.permute(0, 2, 1)
+    t = centroid_B.permute(0,2,1) - R @ centroid_A.permute(0,2,1)
+    # warp_A = transform(A, integrate_trans(R,t))
+    # RMSE = torch.sum( (warp_A - B) ** 2, dim=-1).mean()
+    return integrate_trans(R, t)
